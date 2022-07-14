efa8761571b1c4a4fc783716356c8f9e808b0b47
crisine <mikunyang@gmail.com>
2022-07-14 17:29:52 +0900

    first commit

---
+++ Sample Project/src/testpkg/HelloWorld.java
@@ -0,0 +1,13 @@
+package testpkg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		// a + b의 합이 나타나도록 아래 코드에 반영 부탁드립니다.
+		System.out.println("");
+	}
+
+}
